f8d3a7272d061e537e6a00984bb0919554410ad5
Rich Churcher <rich.churcher@gmail.com>
2015-06-05 15:05:58 +1200

    created board model ( basic schema )

--- models/board.js
+++ models/board.js
@@ -1,8 +1,24 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-var BoardSchema = new Schema({
-  name: String
+var boardSchema = new Schema({
+	bubbles: [{
+		bubbleId: String,
+		heading: String,
+		content: String,
+		location: {
+			top: Number,
+			left: Number
+		},
+		size: {
+			top: Number,
+			left: Number
+		}
+	}],
+	connections: [{
+		startBubbleId: String,
+		endBubbleId: String
+	}]
 });
 
-module.exports = mongoose.model('Board', BoardSchema);
+module.exports = mongoose.model('Board', boardSchema);
