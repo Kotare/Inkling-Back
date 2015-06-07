dcdb5f514651b07b9217d990020814a74192cedb
Rich Churcher <rich.churcher@gmail.com>
2015-06-07 18:51:41 +1200

    CORS: fix PUT/OPTIONS

--- server.js
+++ server.js
@@ -14,8 +14,14 @@ app.use(bodyParser.json());
 
 // CORS
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Content-Length');
+
+  if ('OPTIONS' === req.method) {
+    res.sendStatus(200);
+  }
+
   next();
 });
 
