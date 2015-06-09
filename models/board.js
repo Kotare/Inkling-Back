681c86a7d7ba7e6b148110f933d1b9f15d5a1ea6
Rich Churcher <rich.churcher@gmail.com>
2015-06-09 23:55:41 +1200

    Debug

--- models/board.js
+++ models/board.js
@@ -1,26 +1,30 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+var bubbleSchema = new Schema({
+  type: String,
+  bubbleId: String,
+  sourceUrl: String,
+  content: String,
+  location: {
+    top: Number,
+    left: Number
+  },
+  size: {
+    height: String,
+    width: String
+  }
+});
+
+var connectionSchema = new Schema({
+  startBubbleId: String,
+  endBubbleId: String
+});
+
 var boardSchema = new Schema({
   fbId: String,
-	bubbles: [{
-    type: String,
-		bubbleId: String,
-    sourceUrl: String,
-		content: String,
-		location: {
-			top: Number,
-			left: Number
-		},
-		size: {
-			height: String,
-			width: String
-		}
-	}],
-	connections: [{
-		startBubbleId: String,
-		endBubbleId: String
-	}]
+	bubbles: [bubbleSchema],
+	connections: [connectionSchema]
 });
 
 boardSchema.set('versionKey', false);
